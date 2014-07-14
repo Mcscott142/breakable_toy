2f4ceb640bfc46c738f90bdbb8f960a01d527147
Colin Scott <m.colin.scott@gmail.com>
2014-07-14 16:36:04 -0400

    styling for layout page complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,16 @@ class ApplicationController < ActionController::Base
 
    before_filter :configure_permitted_parameters, if: :devise_controller?
 
+   before_action :set_listings
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name, :city,
       :state, :zip, :country, :profile_pic]
   end
+
+   def set_listings
+    @recentlistings = Listing.order(created_at: :desc ).limit(5)
+   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @listings = Listing.order(created_at: :desc ).limit(10)
   end
 end
